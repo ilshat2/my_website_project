72d379f51e9f8d3aeea1336afc41ad3c43a5775e
ilshat kushmanbetov <ilshat26101993@list.ru>
2024-12-24 19:20:09 +0700

    feat: adding converters

--- siteilshat/myresume/urls.py
+++ siteilshat/myresume/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from myresume import views
+from django.urls import path, re_path, register_converter
+from myresume import views, converters
+
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.home),  # http://127.0.0.1:8000/
-    path('myresume/', views.index),  # http://127.0.0.1:8000/myresume/
+    path('myresume/<int:my_id>/', views.myresume),  # http://127.0.0.1:8000/myresume/123/
+    path('myresume/<slug:my_slug>/', views.myresume_by_slug),  # http://127.0.0.1:8000/myresume/qerty123/
+    path('archive/<year4:year>/', views.archive)
 ]
--- siteilshat/myresume/views.py
+++ siteilshat/myresume/views.py
@@ -2,9 +2,18 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request):  # HttpRequest
-    return HttpResponse('Страница приложения myresume')
-
 
 def home(request):  # HttpRequest
     return HttpResponse('<h1>Главная страница</h1>')
+
+
+def myresume(request, my_id):  # HttpRequest
+    return HttpResponse(f'<h3>Страница приложения myresume</h3><p>id: {my_id}</p>')
+
+
+def myresume_by_slug(request, my_slug):  # HttpRequest
+    return HttpResponse(f'<h3>Страница приложения myresume</h3><p>slug: {my_slug}</p>')
+
+
+def archive (request, year):
+    return HttpResponse(f'<h3>Архив по годам</h3><p>{year}</p>')
