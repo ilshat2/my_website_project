b79aa4b9326df59e294c717b45681f2be63d4a09
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-01-06 17:57:37 +0700

    style(ui): connecting static files

--- siteilshat/siteilshat/settings.py
+++ siteilshat/siteilshat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7kv*k9ang^0$i_h6xx)fk3+bjt80u086wj9zh)8c_8#q59!xdg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'siteilshat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
