f9687088ac1e9b3837acd81024019ef78250a973
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-02-27 20:57:49 +0600

    feat(db): Tags work | Added data grouping when querying the database

--- siteilshat/myresume/templatetags/myresume_tags.py
+++ siteilshat/myresume/templatetags/myresume_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 #import myresume.views as views
 from myresume.models import TagPost, Category
 
@@ -7,9 +8,9 @@ register = template.Library()
 
 @register.inclusion_tag('myresume/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = Category.objects.all()
+    cats = Category.objects.annotate(total=Count('posts')).filter(total__gt=0)
     return {'cats': cats, 'cat_selected': cat_selected}
 
 @register.inclusion_tag('myresume/list_tags.html')
 def show_all_tags():
-    return {'tags': TagPost.objects.all()}
+    return {'tags': TagPost.objects.annotate(total=Count('tags')).filter(total__gt=0)}
