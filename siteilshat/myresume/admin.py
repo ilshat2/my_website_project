e54575f0ff13ec4d29ffb59757a07606aeeaacf4
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-03-05 17:41:16 +0600

    feat(admin): Adding a search bar and filter bar

--- siteilshat/myresume/admin.py
+++ siteilshat/myresume/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin, messages
 from myresume.models import Myresume, Category
 
 
+class PhotoFilter(admin.SimpleListFilter):
+    title = 'Есть ли фото'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('there_photo', 'Есть фото'),
+            ('no_photo', 'Нет фото')
+        ]
+    def queryset(self, request, queryset):
+        if self.value() == 'there_photo':
+            return queryset.filter(photo__isnull=False)
+        elif self.value() == 'no_photo':
+            return queryset.filter(photo__isnull=True)
+
+
 @admin.register(Myresume)
 class MyresumeAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
@@ -10,6 +26,8 @@ class MyresumeAdmin(admin.ModelAdmin):
     list_editable = ('is_published', )
     list_per_page = 5
     actions = ['set_published', 'set_draft']
+    search_fields = ['title__startswith', 'cat__name']
+    list_filter = [PhotoFilter, 'cat__name', 'is_published']
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, myresume: Myresume):
