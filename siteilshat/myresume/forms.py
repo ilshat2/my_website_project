04583eda2732841d437091d2a03a80ac2e21e2f9
ilshat kushmanbetov <ilshat26101993@list.ru>
2025-03-11 15:23:27 +0600

    feat(forms): Update form files and view functions

--- siteilshat/myresume/forms.py
+++ siteilshat/myresume/forms.py
@@ -4,3 +4,8 @@ from myresume.models import Category, Photo
 
 class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255)
+    slug = forms.SlugField(max_length=255)
+    content = forms.CharField(widget=forms.Textarea())
+    is_published = forms.BooleanField()
+    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    photo = forms.ModelChoiceField(queryset=Photo.objects.all())
--- siteilshat/myresume/views.py
+++ siteilshat/myresume/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
+from myresume.forms import AddPostForm
 from myresume.models import Myresume, Category, TagPost
+
 
 
 menu = [
@@ -45,7 +47,13 @@ def show_post(request, post_slug):
 
 
 def addpage(request):
-    return render(request, 'myresume/addpage.html', {'menu': menu, 'title': 'Добавление статьи'})
+    form = AddPostForm()
+    data = {
+        'menu': menu,
+        'title': 'Добавление статьи',
+        'form': form
+    }
+    return render(request, 'myresume/addpage.html', data)
 
 
 def contact(request):
